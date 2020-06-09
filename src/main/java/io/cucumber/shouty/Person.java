07f8411a0d3c83255dc0ea95ca4b6e0842f5d73a
Tom <thomas.spencer@codurance.com>
2020-06-09 14:48:58 +0100

    Add failing cucumber test

---
+++ src/main/java/io/cucumber/shouty/Person.java
@@ -0,0 +1,18 @@
+package io.cucumber.shouty;
+
+import java.util.List;
+
+public class Person {
+
+  public void moveTo(Integer distance) {
+    throw new UnsupportedOperationException("Implement me!");
+  }
+
+  public void shout(String message) {
+    throw new UnsupportedOperationException("Implement me!");
+  }
+
+  public List<String> getMessagesHeard() {
+    throw new UnsupportedOperationException("Implement me!");
+  }
+}
---
+++ src/test/java/io/cucumber/shouty/StepDefinitions.java
@@ -0,0 +1,34 @@
+package io.cucumber.shouty;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+
+  private Person sean;
+  private Person lucy;
+  private String messageFromSean;
+
+  @Given("Lucy is located {int} metres from Sean")
+  public void lucy_is_located_metres_from_Sean(Integer distance) {
+    lucy = new Person();
+    sean = new Person();
+    lucy.moveTo(distance);
+
+
+  }
+
+  @When("Sean shouts {string}")
+  public void sean_shouts(String message) {
+    sean.shout(message);
+  }
+
+  @Then("Lucy hears Sean's message")
+  public void lucy_hears_Sean_s_message() {
+    assertEquals(asList(messageFromSean), lucy.getMessagesHeard());
+  }
+}
